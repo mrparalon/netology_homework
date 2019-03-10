26481217e5afb3be1512eb3c405acda68515011c
mr.paralon <mr.paralon@gmail.com>
2019-03-10 21:06:21 +0300

    adv_print for homework_1_4 fixed

--- python_adv/homework_1_4/adv_print.py
+++ python_adv/homework_1_4/adv_print.py
@@ -1,7 +1,20 @@
-def adv_print(value, start='', in_file = False, **kwargs):
+import sys
+from io import StringIO
+
+def adv_print(*args, start='', in_file = False, **kwargs):
+    """
+    Get max_line and all arguments used in built-in print function as kwargs.
+    """
+    max_line = kwargs.pop('max_line', False)
+    print(kwargs)
+    old_stdout = sys.stdout
+    value = StringIO()
+    sys.stdout = value
+    print(*args, **kwargs)
+    sys.stdout = old_stdout
+    value = value.getvalue()
     value = start + value
-    if 'max_line' in kwargs:
-        max_line = kwargs['max_line']
+    if max_line:
         value = value[:max_line] + '\n' + value[max_line:]
     if in_file:
         if 'filename' in kwargs:
@@ -19,4 +32,4 @@ if __name__ == "__main__":
     adv_print(value, start="Test")
     adv_print(value, max_line=6)
     adv_print(value, in_file=True, filename='test.txt')
-    adv_print(value, start='New Test ', max_line = 9, in_file=True)
+    adv_print(1, 2, 3, ['a', 'v'], start='New Test ', max_line = 9, in_file=True, sep=' 0_0 ', end='XxX')
--- python_adv/homework_1_4/phone_book.py
+++ python_adv/homework_1_4/phone_book.py
@@ -79,3 +79,4 @@ if __name__ == '__main__':
 
     print('\n_________проверка удаления_________\n'.upper())
     test_phone_book.get_all_contacts()
+
